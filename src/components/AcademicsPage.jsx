52e8ac2df47dc2df432de50b61a9db0611d7d334
Jingheng Wang <33430853+JimmyCHWang@users.noreply.github.com>
2023-05-31 12:44:28 -0700

    Academics Page w/ GPA and Toolbar

--- src/components/AcademicsPage.jsx
+++ src/components/AcademicsPage.jsx
@@ -1,10 +1,36 @@
-import { Box, Button, Typography, Container, Chip } from '@mui/material';
+import { Box, Button, Typography, Container, Grid, Chip, CircularProgress, Switch } from '@mui/material';
 import Layout from './Layout';
 
 import courseList from './CourseList';
-import { DataGrid } from '@mui/x-data-grid';
+import { DataGrid, GridToolbarContainer, GridToolbarColumnsButton, GridToolbarFilterButton } from '@mui/x-data-grid';
+import { useState } from 'react';
+
+function CircularProgressWithLabel(props) {
+    return (
+        <Box sx={{ position: 'relative', display: 'inline-flex', marginY: '8px' }}>
+            <CircularProgress variant="determinate" {...props} />
+            <Box
+                sx={{
+                    top: 0,
+                    left: 0,
+                    bottom: 0,
+                    right: 0,
+                    position: 'absolute',
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                }}
+            >
+                <Typography variant="caption" component="div" sx={props.textsx}>
+                    {props.text}
+                </Typography>
+            </Box>
+        </Box>
+    );
+}
 
 const AcademicsPage = () => {
+    const [detailed, setDetailed] = useState(false);
     const termToText = (term) =>
         ({
             0: '',
@@ -13,6 +39,60 @@ const AcademicsPage = () => {
             3: 'Summer',
             4: 'Fall',
         }[term]);
+    const simplifiedColumns = [
+        {
+            field: 'term',
+            headerName: 'Term',
+            description: 'School Term',
+            sortable: false,
+            width: 100,
+            valueGetter: (params) => `20${params.row.year} ${termToText(params.row.term)}`,
+        },
+        {
+            field: 'course_name',
+            headerName: 'Name',
+            description: 'Course Name',
+            sortable: false,
+            width: 300,
+        },
+        {
+            field: 'institution',
+            headerName: 'Institution',
+            width: 100,
+        },
+        {
+            field: 'grade',
+            headerName: 'Grade',
+            width: 60,
+        },
+        {
+            field: 'technology',
+            headerName: 'Technologies',
+            description: 'List of technologies used in the course',
+            width: 350,
+            sortable: false,
+            renderCell: (param) => {
+                const techString = param.row.technology;
+                const splitString = techString.split('|');
+                const keyPrefix = param.row.course_dept + param.row.course_id;
+                const chips = splitString.map((s) => <Chip key={keyPrefix + s} label={s} variant="outlined" />);
+                return (
+                    <Box
+                        sx={{
+                            minHeight: '52px',
+                            py: '4px',
+                            width: 350,
+                            display: 'flex',
+                            flexWrap: 'wrap',
+                            alignItems: 'center',
+                        }}
+                    >
+                        {chips}
+                    </Box>
+                );
+            },
+        },
+    ];
     const columns = [
         {
             field: 'term',
@@ -51,18 +131,19 @@ const AcademicsPage = () => {
         {
             field: 'units',
             headerName: 'Units',
-            width: 50,
+            width: 60,
         },
         {
             field: 'grade',
             headerName: 'Grade',
-            width: 50,
+            width: 60,
         },
         {
             field: 'technology',
             headerName: 'Technologies',
             description: 'List of technologies used in the course',
             width: 350,
+            sortable: false,
             renderCell: (param) => {
                 const techString = param.row.technology;
                 const splitString = techString.split('|');
@@ -109,13 +190,59 @@ const AcademicsPage = () => {
             },
         },
     ];
+    const useColumn = detailed ? columns : simplifiedColumns;
+    const toggleDetailed = () => {
+        setDetailed((detailed) => !detailed);
+    };
+    const customToolbar = () => (
+        <GridToolbarContainer>
+            <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center', marginLeft: '10px' }}>
+                <Typography>Simplified</Typography>
+                <Switch checked={detailed} color="default" onChange={toggleDetailed} />
+                <Typography>Detailed</Typography>
+            </Box>
+            <GridToolbarColumnsButton />
+            <GridToolbarFilterButton />
+            <Box sx={{ flexGrow: 1 }}></Box>
+            <Box>
+                <Typography variant="caption">Use desktop for best performance</Typography>
+            </Box>
+        </GridToolbarContainer>
+    );
     return (
         <Layout>
-            <Typography>Academics Page</Typography>
+            <Container sx={{ textAlign: 'center', marginY: '20px' }}>
+                <Grid container>
+                    <Grid item xs={12} md={6}>
+                        <Typography variant="h6">M.S. ECE @ CMU</Typography>
+                        <Typography>QPA</Typography>
+                        <CircularProgressWithLabel
+                            variant="determinate"
+                            value={98.25}
+                            size={200}
+                            text={'3.93 / 4.00'}
+                            textsx={{ fontSize: 24 }}
+                        />
+                        <Typography>Top 5% (estimated)</Typography>
+                    </Grid>
+                    <Grid item xs={12} md={6}>
+                        <Typography variant="h6">B.S. CS / B.A. NDT @ UCSC</Typography>
+                        <Typography>GPA</Typography>
+                        <CircularProgressWithLabel
+                            variant="determinate"
+                            value={98}
+                            size={200}
+                            text={'3.92 / 4.00'}
+                            textsx={{ fontSize: 24 }}
+                        />
+                        <Typography>Top 2% (Honor: Summa Cum Laude)</Typography>
+                    </Grid>
+                </Grid>
+            </Container>
             <Container>
                 <DataGrid
                     rows={courseList}
-                    columns={columns}
+                    columns={useColumn}
                     initialState={{
                         pagination: {
                             paginationModel: { page: 0, pageSize: 10 },
@@ -124,6 +251,7 @@ const AcademicsPage = () => {
                     pageSizeOptions={[5, 10, 20]}
                     getRowId={(row) => row.course_dept + row.course_id}
                     getRowHeight={() => 'auto'}
+                    slots={{ toolbar: customToolbar }}
                 />
             </Container>
         </Layout>
